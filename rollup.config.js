a9951bf81118e1c006fa6acfb27d686e3e522505
jprochazk <honza.spacir@gmail.com>
2020-08-11 16:00:59 +0200

    updated rollup to use terser

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,22 @@
 //import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
 import pkg from "./package.json";
+//import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 export default [
     // UMD, CJS, ESM
     {
         input: "src/index.ts",
         plugins: [
+            //resolve(),
             typescript({
                 typescript: require("typescript"),
+            }),
+            terser({
+                output: {
+                    comments: false,
+                },
             }),
         ],
         output: [
